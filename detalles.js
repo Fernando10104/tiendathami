9093982d371d9cd5e4b5a255c0482f6755e3f08e
Fernando10104 <carinafaviola85@gmail.com>
2025-05-14 14:32:42 -0300

    Añadir nuevos archivos y actualizar existentes

---
+++ detalles.js
@@ -0,0 +1,64 @@
+// Cargar los productos desde el archivo JSON
+fetch("./src/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        const productos = data.productos;
+        const params = new URLSearchParams(window.location.search);
+        const id = params.get('id');  // <-- ID del producto
+        
+        console.log(typeof id, "id del producto");
+        mostrarProducto(productos,id);
+    })
+    .catch(error => console.error('Error al cargar los productos:', error));
+
+    // Esta línea selecciona el contenedor donde se mostrarán los detalles del producto en el archivo producto-detallado.html
+const contenedor_detalles = document.querySelector(".contenedor-principal");
+
+
+// funcion de mostrar productos detallados
+function mostrarProducto(productos,id){
+    console.log(Array.isArray(productos), "productos");
+    console.log(id);
+    const idInt = parseInt(id);  // convierte el string a número entero
+    console.log(typeof idInt, "id del producto");
+    console.log(productos,"productos del mostrar");
+    const producto = productos.find(producto => producto.id === idInt);
+    console.log(producto, "producto encontrado");
+    const div = document.createElement("div");
+    div.classList.add("contenedor-productos");
+    div.innerHTML =`
+                    <div class="contenedor-img-producto">
+                        <img class="img-producto" src="${producto.image}" alt="${producto.nombre}">
+                    </div>
+                    <div class="info-producto-destalles">
+                        <hr style="height: 3px; background-color: #f5cbc9; border: none; width: 350px; margin: 5px 0;">
+                        <h1>${producto.nombre}</h1>
+                        <h2>${producto.nombre2}</h2>
+                        <hr style="height: 3px; background-color: #f5cbc9; border: none; width: 350px; margin: 5px 0;">
+
+                        <h3 class="cantidad">${producto.cantidad}</h3>
+                        
+                        <p class="descripcion">INCLUYE:
+                        ${producto.descripcion}
+                        </p>
+                        <hr style="height: 3px; background-color: #f5cbc9; border: none; width: 350px; margin: 5px 0;">
+                        <p class="precio-detalles">Precio: ${producto.precio} Gs</p>
+                        <!-- Botón de WhatsApp -->
+                        <a href="https://wa.me/595985692386" target="_blank" class="boton-whatsapp">
+                        <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" alt="WhatsApp" class="icono-whatsapp">
+                        Hacer el pedido
+                        </a>
+
+                    </div>
+                    </div>
+                        <hr style="height: 1px; background-color: #ffeeee; border: none; width: 100%;">
+                        <div class="descripcion-producto">
+                                <h1>Detalles:</h1>
+                                <p>${producto.detalles}</p>
+                                
+
+                    </div> `
+                    ;
+                    contenedor_detalles.appendChild(div);
+ }
+
--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-
+const opciones = {
+  keys: ['nombre'],
+  threshold: 0.4
+};
 // Cargar los productos desde el archivo JSON
 fetch("./src/productos.json")
     .then(response => response.json())
     .then(data => {
         const productos = data.productos;
-        
-        console.log(productos);
-        console.log(productos[1].imagen);
         cargarproductos(productos);
+        const fuse = new Fuse(productos, opciones);
+        const input = document.getElementById("search");
+        input.addEventListener("input", () => {
+        const texto = input.value.trim();
+        if (texto === ""){
+            cargarproductos(productos);
+            console.log(productos,"lololololololo")
+            return;
+
+            
+        }
+
+            const resultado = fuse.search(texto);
+            const filtrados = resultado.map(r => r.item);
+            cargarproductos(filtrados);
+
+        });
+     
     })
     .catch(error => console.error('Error al cargar los productos:', error));
+
+
 
 
 
@@ -17,28 +37,27 @@ fetch("./src/productos.json")
 const contenedorproductos = document.querySelector("#contenedor-productos");
 
 function cargarproductos(productos){
-    productos.forEach(producto => {
+    contenedorproductos.innerHTML = ""; // limpiar lo anterior
+    productos.forEach( item  => {
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
 
-            <img class="producto-imagen" src="${producto.image}" alt="${producto.nombre}">
+            <img class="producto-imagen" src="${item.image}" alt="${item.nombre}">
             <div class="producto-info">
-                <h3 class="producto-nombre">${producto.nombre}</h3>
-                <p>Precio: ${producto.precio} Gs.</p>
-                <button class="btn-primary" id="${producto.id}">Agregar al carrito</button>
+                <h3 class="producto-nombre">${item.nombre}</h3>
+                <p>Precio: ${item.precio} Gs.</p>
+                <button class="btn-primary" id="${item.id}">Agregar al carrito</button>
             </div>
         `;
+        div.onclick = () => {
+            window.location.href = `producto-detallado.html?id=${item.id}`;
+        };
         contenedorproductos.appendChild(div);
     })
 
 }
 
-// cargarproductos is now called with the productos array from the fetch response
 
-const contenedor = document.querySelector("#contenedor-producto");
-// funcion de mostrar productos detallados
-function mostrarProducto(id){
-    const producto = productos.find(producto => producto.id === id);
-    const contenedor = document.querySelector("#contenedor-producto");
-}
+
+
